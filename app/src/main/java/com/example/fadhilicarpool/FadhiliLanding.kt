d64bf56483da841ad495598d6560a10bde358a8d
Kip <60607432+lembuss@users.noreply.github.com>
2020-05-14 02:37:33 +0300

    update

--- app/src/main/java/com/example/fadhilicarpool/FadhiliLanding.kt
+++ app/src/main/java/com/example/fadhilicarpool/FadhiliLanding.kt
@@ -1,8 +1,10 @@
 package com.example.fadhilicarpool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_fadhili_landing.*
 
 class FadhiliLanding : AppCompatActivity() {
 
@@ -12,5 +14,15 @@ class FadhiliLanding : AppCompatActivity() {
 
 //             hide status bar
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+//        click listeners for the buttons
+
+        btnSignIn.setOnClickListener {
+            startActivity(Intent(this, FadhiliSignIn::class.java))
+        }
+
+        btnSignUp.setOnClickListener {
+            startActivity(Intent(this,  FadhiliRegister:: class.java))
+        }
     }
 }
